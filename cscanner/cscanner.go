34cbde6f850a391403795b58a224e7abf9e4dda6
Bhargav Srinivasan <bsvasan92@icloud.com>
2021-07-13 11:03:05 -0700

    completed file parser, with a dummy safeOpen function to parse /proc/net/tcp

--- cscanner/cscanner.go
+++ cscanner/cscanner.go
@@ -4,6 +4,8 @@ import (
 	"log"
 )
 
+// ConnectionScanner returns a map of tcp connections from the input tokens
+// that have a unique(srcIP:srcPort, dstIP:dstPort).
 func ConnectionScanner() {
 	log.Printf("logic to count / filter tokens after parsing")
 }
--- fparser/fparser_test.go
+++ fparser/fparser_test.go
@@ -1,11 +1,19 @@
 package fparser
 
 import (
+	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFileParser1(t *testing.T) {
-
+func TestFileParserFunctional1(t *testing.T) {
+	result, err := FileParser(1, "temp", 6, " ")
+	if err != nil {
+		log.Printf("TestFileParserFunctional1 failed")
+		assert.Fail(t, "TestFileParserFunctional1 failed")
+	}
+	assert.Contains(t, result[1][3], "AddAC11:FDRE")
 }
 
 func TestFileParser2(t *testing.T) {
